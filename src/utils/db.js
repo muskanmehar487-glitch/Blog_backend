b2f384956369b67ab6977b93a915a0c34e9cca2b
muskan <muskanmehar487@gmail.com>
2025-12-12 15:11:31 +0500

    commit

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
 export async function connectToDatabase() {
-  const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/blog_app';
+  const mongoUri = process.env.MONGODB_URI;
+  if (!mongoUri) {
+    throw new Error('MONGODB_URI is not set');
+  }
   mongoose.set('strictQuery', true);
   await mongoose.connect(mongoUri, { 
     autoIndex: true,
